a93d7dccbab391ae96831c867a42bc39c6ead9a4
Lasma Poksane <laaasma.p@gmail.com>
2023-03-10 00:12:42 +0100

    create static BusinessList component

---
+++ src/components/BusinessList/BusinessList.js
@@ -0,0 +1,19 @@
+import { Component } from "react";
+import Business from "../Business/Business";
+
+class BusinessList extends Component {
+  render() {
+    return (
+      <div>
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+        <Business />
+      </div>
+    );
+  }
+}
+
+export default BusinessList;
