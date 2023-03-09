304e06809a6a7ee3fa619d9c7a49d802cbc47a62
Lasma Poksane <laaasma.p@gmail.com>
2023-03-10 00:47:33 +0100

    render SearchBar component

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,12 +2,14 @@ import "./App.css";
 import "./App.css";
 import BusinessList from "../BusinessList/BusinessList";
 import { Component } from "react";
+import SearchBar from "../SearchBar/SearchBar";
 
 class App extends Component {
   render() {
     return (
       <div className="app">
         <h1>ravenous</h1>
+        <SearchBar />
         <BusinessList />
       </div>
     );
