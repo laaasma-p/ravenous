4bf9c4d51c876877420bf8b079118e25d73d2c37
Lasma Poksane <laaasma.p@gmail.com>
2023-03-10 00:41:53 +0100

    build initial structure for communication with API

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,12 +1,25 @@
 import { Component } from "react";
 import "./SearchBar.css";
 
+const sortByOptions = {
+  "Best Match": "best_match",
+  "Highest Rated": "rating",
+  "Most Reviewed": "review_count",
+};
+
 class SearchBar extends Component {
+  renderSortByOptions() {
+    return Object.keys(sortByOptions).map((sortByOption) => {
+      let sortByOptionValue = sortByOptions[sortByOption];
+      return <li key={sortByOptionValue}>{sortByOption}</li>;
+    });
+  }
+
   render() {
     return (
       <div className="searchbar">
         <div className="searchbar-sort-options">
-          <ul>Sort Options</ul>
+          <ul>{this.renderSortByOptions()}</ul>
         </div>
         <div className="searchbar-fields">
           <input placeholder="Search Businesses" />
