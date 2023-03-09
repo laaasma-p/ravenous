4eff65153336c2a38aa2ea2798cc742aa59f41cb
Lasma Poksane <laaasma.p@gmail.com>
2023-03-10 00:35:33 +0100

    style SearchBar component

--- src/components/SearchBar/SearchBar.js
+++ src/components/SearchBar/SearchBar.js
@@ -1,17 +1,18 @@
 import { Component } from "react";
+import "./SearchBar.css";
 
 class SearchBar extends Component {
   render() {
     return (
-      <div>
-        <div>
+      <div className="searchbar">
+        <div className="searchbar-sort-options">
           <ul>Sort Options</ul>
         </div>
-        <div>
+        <div className="searchbar-fields">
           <input placeholder="Search Businesses" />
           <input placeholder="Where?" />
         </div>
-        <div>
+        <div className="searchbar-submit">
           <a href="link.com">Let's Go</a>
         </div>
       </div>
